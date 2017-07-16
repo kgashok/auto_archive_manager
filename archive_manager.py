196bd951520efb06edbf2d4812ca084f5cc89198
Stanley Ulili <stanleyulili@gmail.com>
2017-07-16 22:09:58 +0200

    halfway to adding function to delete zipped files after extraction

--- archive_manager.py
+++ archive_manager.py
@@ -2,14 +2,25 @@ import os
 import zipfile
 
 
+dir_location = []
+
+
 def get_dir():
     print('''Enter the location with zipped file
         for example:  /home/username/Documents/zipped''')
     location = input('..')
     if location:
-        return location
+        dir_location.append(location)
     else:
         print("Please enter the location")
+
+
+def delete_zipped_files():
+    location = get_dir()
+
+    for file in os.listdirs(location):
+        if file.endswith('.zip'):
+            print(file)
 
 
 def unzip_files():
@@ -21,6 +32,15 @@ def unzip_files():
             file_unzipped.extractall()
             file_unzipped.close()
             print(zipped_file + " successfuly extracted.")
+
+    print('''Do you want to move or deleted the zipped files?
+                Enter : 1. Delete all zipped files
+                        2. Move all zipped files into one folder
+                        3. Do nothing
+            ''')
+    choice = int(input('..'))
+    if choice == 1:
+        delete_zipped_files()
 
 
 def get_action():
